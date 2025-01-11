2729704c2c813cf73c2ba273e3d8372db454e2c9
discgrid2025 <discgrid@gmail.com>
2025-01-11 13:54:17 +0100

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,12 @@
+# x = 50
+# y = 1
+
+# while y < 50:
+#     print("welcome")
+
+x = 10
+y = 1
+
+while y < x:
+    print("welcome")
+    y+=1
---
+++ python.py
@@ -0,0 +1,54 @@
+names = ["John", "paul", "George"]
+print(dir(names))
+new_names = ["vincent", "james"]
+names.append(True)
+# names.extend("Joe")
+# names.extend(new_names)
+# names.append(new_names)
+# names.insert(2, "Joe")
+# names.clear()
+# del(names)
+# another_names = names.copy()
+# print(names)
+# print(another_names)
+names = ["john" , "john" ,"paul" ,"george" ,]
+name = "john"
+# count_result = names.count(name)
+# 
+names.pop()
+# popped = names.pop(2)
+# print(popped)
+# # print(names)
+# names.remove("paul")
+# print(names)
+
+    #    bmw     audi     toyota    benz
+cars = [["M1","M6"], "audi", "toyota", ["amg", "Gle"]]
+# print(cars[3])
+# print(f"i am riding a (car)(-1) (1)")
+# cars[0][1] = "M1"
+# print(cars) "audi"
+
+
+cars = {"bmw": "M5" , "audi": "audi" , "number": 4, "status": True}
+cars = {
+    "bmw": "M5" , 
+    "audi": "audi" , 
+    "number": 4, 
+    "status": True
+    }
+# cars ["status"] = False
+# our_keys = cars.keys()
+# print(our_keys)
+
+
+# our_keys = list (cars.keys())
+# print(our_keys)
+# print(len(cars))
+# print(dir(cars))
+
+our_values = list(cars.values())
+print(our_values)
+
+# print(cars["bmw"])
+# print(cars[0][1]) 
